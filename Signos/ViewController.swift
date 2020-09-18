df141cba00d859ed58d1bf9df61460b261bd014e
Dan Negreiros <negreiros.dev@hotmail.com>
2020-09-18 11:33:02 -0300

    Initial Commit

---
+++ Signos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Signos
+//
+//  Created by Daniel Negreiros on 18/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
